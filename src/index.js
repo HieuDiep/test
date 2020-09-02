545ce9899206ef236e0d834af3ff49182a74d8b0
Diệp Văn Hiếu <41156680+HieuZenKa@users.noreply.github.com>
2020-09-02 16:35:29 +0700

    yes

--- src/index.js
+++ src/index.js
@@ -9,14 +9,21 @@ const path = require('path')
 app.use(morgan('combined'))
 
 //template engine
-app.engine('handlebars', handlebars())
-app.set('view engine', 'handlebars');
+app.engine('hbs', handlebars({
+  extname :'.hbs'
+}))
+
+app.set('view engine', 'hbs');
 
 app.set('views',path.join(__dirname,'resources/views'))
 //console.log('PATH: ',path.join(__dirname,'resources/views'))
 
 app.get('/', (req, res) => {
   res.render('home');
+})
+
+app.get('/news', (req, res) => {
+  res.render('news');
 })
 
 app.listen(port, () => {
