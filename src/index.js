e74f1e98250b8d57807e6535696a2d7905e92ce4
Diệp Văn Hiếu <41156680+HieuZenKa@users.noreply.github.com>
2020-09-03 17:08:57 +0700

    các thao tác post cơ ban

--- src/index.js
+++ src/index.js
@@ -2,29 +2,33 @@ const express = require('express')
 const app = express()
 const port = 3000
 const morgan = require('morgan')
-var handlebars = require('express-handlebars');
+const handlebars = require('express-handlebars');
 const path = require('path')
 
 //http logger
-app.use(morgan('combined'))
+//app.use(morgan('combined'))
 
 //template engine
 app.engine('hbs', handlebars({
   extname :'.hbs'
 }))
 
-app.set('view engine', 'hbs');
+app.set('view engine', 'hbs')
+
+//static photo
+app.use(express.static(path.join(__dirname,'public')))
+
 
 app.set('views',path.join(__dirname,'resources/views'))
-//console.log('PATH: ',path.join(__dirname,'resources/views'))
 
 app.get('/', (req, res) => {
   res.render('home');
 })
 
-app.get('/news', (req, res) => {
-  res.render('news');
+app.post('/',(req,res) =>{
+  res.render('news')
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
